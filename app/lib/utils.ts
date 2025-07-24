41dc13639a91cb8e84efc368b8c852750e4bbe60
meghana-meher <meghanameher.tenneti@gmail.com>
2025-07-24 16:55:39 +0530

    Resume Feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,10 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function formatSize(bytes: number): string {
   if (bytes === 0) return '0 Bytes';
 
@@ -11,4 +18,4 @@ export function formatSize(bytes: number): string {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
 
-export const generateUUID =()=> crypto.randomUUID();
+export const generateUUID = () => crypto.randomUUID();
